a6be5ba2706b3d1be0868e23a3f6f363010447f0
Stracciatella <105241465+IoStracciatella@users.noreply.github.com>
2024-11-05 05:50:16 -0300

    Método de Jacobi

--- exercicio3.py
+++ exercicio3.py
@@ -1,38 +1,46 @@
-#for Jacobi method
+# Método de Jacobi
+
 import numpy as np
-import numpy.linalg as al
-import copy
+import matplotlib.pyplot as plt
 
-print('-' * 30)
-print('Método de Jacobi')
-print('-' * 30)
+# Valores iniciais que serão inseridos na função
+x0, y0 = 0, 0
+x0 = float(input('Escolha um valor inicial de x para iniciar a aproximação. n: '))
+y0 = float(input('Escolha um valor inicial de x para iniciar a aproximação. n: '))
 
-A=np.array([[10.,-1.,2.,0.],[-1.,11.,-1.,3.],[2.,-1.,10.,-1.],[0.,3.,-1.,8.]])
-b=np.array([6.,25.,-11.,15.])
-n=4
-x0=np.array([0.,0.,0.,0.])
-x=copy.deepcopy(x0)
-NO=1000
-Tol=1.e-3
-k=1
+def metodoDeJacobi(x0, y0, tol):
+    x, y = 0, 0 # Essas variáveis são os valores que serão calculados, e recalculados, e recalculados... até a aproximar o bastante da solução
+    x, y = x0, y0
+    iteracoes = 0
 
-while (k<NO):
-    for i in range(n): #i=0,1,2,...n-1
-        #summation of aij * X0j
-        sum_a=0.0
-        for j in range(n): #j=0,1,2,...n-1
-            if j!=i:
-                sum_a=sum_a+A[i][j]*x0[j]
-        x[i]=(b[i]-sum_a)/A[i][i]
-    
-    diff_inf=al.norm(x-x0,np.inf)/al.norm(x,np.inf)
-    print(k, "ª iteração", x)
-    if (diff_inf<Tol):
-        break
-    k=k+1
-    x0=copy.deepcopy(x)
+    while True:
+        # Calcula novos valores de x e y usando a iteração de Jacobi
+        x_novo = (2 + y) / 3
+        y_novo = (1 + 2 * x) / 4
 
-if (k==NO+1):
-    print("O método falhou após pois não há iterações!")
-else:
-    print("Método concluído com sucesso :D")
+        # Critério de parada com a tolerância dada, ou seja, se ele aproximou a solução do sistema o suficiente da resposta exata
+        if abs(x_novo - x) < tol and abs(y_novo - y) < tol:
+            break
+
+        # Atualiza os valores de x e y
+        x, y = x_novo, y_novo
+        iteracoes += 1
+
+    return iteracoes
+
+tolerancias = [10**-i for i in range(2, 9)] # Valores de tolerância, a gente vai usar pra iterar a função pra cada elemento dessa lista
+listaDeIteracoes = [] # Lista pra armazenar a quantidade de iterações usadas para aproximar o resultado para cada um dos erros
+
+# Executa o método de Jacobi para cada tolerância. "tol" assume o valor de 1 valor de "tolerancias" a cada iteração
+for tol in tolerancias:
+    iteracoes = metodoDeJacobi(x0, y0, tol)
+    listaDeIteracoes.append(iteracoes)
+
+# Plota o gráfico
+plt.plot(tolerancias, listaDeIteracoes, marker='o')
+plt.xscale('log')
+plt.xlabel('Erro (ε)')
+plt.ylabel('Número de Iterações')
+plt.title('Número de Iterações x Erro no Método de Jacobi')
+plt.gca().invert_xaxis()
+plt.show()
